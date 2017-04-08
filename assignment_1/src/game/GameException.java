90a66d6ae822cdcbb704f0d3daeef05c175564a8
cicicheng <c14cheng@gmail.com>
2017-04-08 14:40:43 +1000

    Update GameException.java

--- assignment_1/src/game/GameException.java
+++ assignment_1/src/game/GameException.java
@@ -1,5 +1,17 @@
-package game;
+ackage game;
 
-public class GameException {
+/**
+ * Game Exception.
+ */
+@SuppressWarnings("serial")
+public class GameException extends IllegalStateException {
 
+    /**
+     * Constructor.
+     *
+     * @param message Exception message.
+     */
+    public GameException(String message) {
+        super(message);
+    }
 }
