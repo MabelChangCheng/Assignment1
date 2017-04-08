09fc4b37c8786d2c68945841cc2fc5d3ee180a37
s3622466 <s3622466@student.rmit.edu.au>
2017-04-08 15:49:02 +1000

    Update Swimmer.java

--- assignment_1/src/participant/Swimmer.java
+++ assignment_1/src/participant/Swimmer.java
@@ -1,5 +1,34 @@
 package participant;
 
-public class Swimmer {
+import game.EventType;
 
+/**
+ * Swimmer.
+ */
+public class Swimmer extends Athlete {
+
+    /**
+     * Constructor.
+     *
+     * @param id    id of the athlete.
+     * @param name  name of the athlete.
+     * @param age   age of the athlete.
+     * @param state state of the athlete.
+     */
+    public Swimmer(int id, String name, int age, String state) {
+        super(id, name, age, state);
+    }
+
+    @Override
+    public boolean canPlay(EventType type) {
+        return type == EventType.SWIMMING;
+    }
+
+    /**
+     * @return a string represents the participant.
+     */
+    @Override
+    public String toString() {
+        return "[Swimmer] " + super.toString();
+    }
 }
