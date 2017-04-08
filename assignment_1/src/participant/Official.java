8922cffe0dd83d7ae1dce96e86c0a14d85e45954
s3622466 <s3622466@student.rmit.edu.au>
2017-04-08 15:44:14 +1000

    Update Official.java

--- assignment_1/src/participant/Official.java
+++ assignment_1/src/participant/Official.java
@@ -1,5 +1,27 @@
 package participant;
 
-public class Official {
+/**
+ * Game official.
+ */
+public class Official extends Participant {
 
+    /**
+     * Constructor.
+     *
+     * @param id    id of the official.
+     * @param name  name of the official.
+     * @param age   age of the official.
+     * @param state state of the official.
+     */
+    public Official(int id, String name, int age, String state) {
+        super(id, name, age, state);
+    }
+
+    /**
+     * @return a string represents the participant.
+     */
+    @Override
+    public String toString() {
+        return "[Official] " + super.toString();
+    }
 }
